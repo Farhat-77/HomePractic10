4d8278e8fab27e9c9d7fd68ee42937b297c92472
Bola_sw25@student.itstep.kz <Bola_sw25@student.itstep.kz>
2021-02-15 10:41:39 +0600

    Добавьте файлы проекта.

---
+++ HomePractic10/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace HomePractic10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee[] employeers;
+            Console.WriteLine("Введите размерность массива");
+            int a = int.Parse(Console.ReadLine());
+            int n = Convert.ToInt32(a);
+            employeers = new Employee[n];
+            for (int i = 0; i < n; i++)
+            {
+                employeers[n] = new Employee();
+                Console.WriteLine("Введите имя");
+                employeers[n].name = Console.ReadLine();
+                Console.WriteLine("Введите зарплату");
+                employeers[n].Salary = Console.ReadLine();
+                Console.WriteLine("Введите вакансию");
+                employeers[n].vacancy = Console.ReadLine();
+                Console.WriteLine("Введите дату принятия на работу");
+                employeers[n].DateOfStartWork = Console.ReadLine();
+            }
+        }
+    }
+    struct Employee 
+    {
+        public enum Vacancies 
+        {
+            Manager, Boss, Clerk, Salesman
+        }
+        public string name;
+        public int Salary;
+        public int[] DateOfStartWork;
+        public Vacancies vacancy;
+    }
+
+}
